1f52f1b103d86b0ab7b4d7dd12fc030cf62e2377
canal6hn <64667960+canal6hn@users.noreply.github.com>
2020-09-01 14:45:46 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,15 +12,6 @@ else
     sed -i 's|#facebook| |g' /etc/nginx/nginx.conf 
 fi
 
-if [ -n "${RESTREAM_KEY}" ]; then
-	echo "Restream.io activated"
-	RESTREAM_KEY_esc=$(echo "$RESTREAM_KEY" | sed 's/[\*\.&]/\\&/g')
-	sed -i 's|#restream|push '"$RESTREAM_URL""$RESTREAM_KEY_esc"';|g' /etc/nginx/nginx.conf 
-else 
-    echo "No Restream.io stream key set... disabling"
-    sed -i 's|#restream| |g' /etc/nginx/nginx.conf 
-fi
-
 if [ -n "${YOUTUBE_KEY}" ]; then
 	echo "YouTube activated"
 	YOUTUBE_KEY_esc=$(echo "$YOUTUBE_KEY" | sed 's/[\*\.&]/\\&/g')
@@ -30,24 +21,14 @@ else
     sed -i 's|#youtube| |g' /etc/nginx/nginx.conf 
 fi
 
-if [ -n "${TWITCH_KEY}" ]; then
-	echo "Twitch.tv activated"
-	TWITCH_KEY_esc=$(echo "$TWITCH_KEY" | sed 's/[\*\.&]/\\&/g')
-	sed -i 's|#twitch|push '"$TWITCH_URL""$TWITCH_KEY_esc"';|g' /etc/nginx/nginx.conf 
+if [ -n "${PERISCOPE_KEY}" ]; then
+	echo "Periscope.tv activated"
+	PERISCOPE_KEY_esc=$(echo "$PERISCOPE_KEY" | sed 's/[\*\.&]/\\&/g')
+	sed -i 's|#periscope|push '"$PERISCOPE_URL""$PERISCOPE_KEY_esc"';|g' /etc/nginx/nginx.conf 
 else 
-    echo "No Twitch.tv stream key set... disabling"
-    sed -i 's|#twitch| |g' /etc/nginx/nginx.conf 
+    echo "No Periscope.tv stream key set... disabling"
+    sed -i 's|#periscope| |g' /etc/nginx/nginx.conf 
 fi
-
-if [ -n "${MIXCLOUD_KEY}" ]; then
-	echo "Mixcloud activated"
-	MIXCLOUD_KEY_esc=$(echo "$MIXCLOUD_KEY" | sed 's/[\*\.&]/\\&/g')
-	sed -i 's|#mixcloud|push '"$MIXCLOUD_URL""$MIXCLOUD_KEY_esc"';|g' /etc/nginx/nginx.conf 
-else 
-    echo "No Mixcloud stream key set... disabling"
-    sed -i 's|#mixcloud| |g' /etc/nginx/nginx.conf 
-fi
-
 
 stunnel4
 
